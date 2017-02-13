adbee7fba0d46b62ce39c947c254a6c2745a20b3
kalle saas <kalle@easypep.de>
2017-02-13 10:18:42 +0100

    Absence

--- spec/staffomatic/client/absences_spec.rb
+++ spec/staffomatic/client/absences_spec.rb
@@ -8,17 +8,17 @@ describe Staffomatic::Client::Absences do
   end
 
   describe ".all_absences", :vcr do
-    it "returns all Staffomatic absences" do
+    fit "returns all Staffomatic absences" do
       absences = @client.all_absences
       expect(absences).to be_kind_of Array
     end
 
-    it "returns all location absences" do
-      absences = @client.location_absences(64)
+    fit "returns all location absences" do
+      absences = @client.location_absences(24086)
       expect(absences).to be_kind_of Array
     end
 
-    it "returns filtered absences" do
+    fit "returns filtered absences" do
       absences = @client.all_absences(:state => 'approved')
       expect(absences).to be_kind_of Array
     end
@@ -44,28 +44,28 @@ describe Staffomatic::Client::Absences do
     # * accept        (scheduler)        Accept! applied absence
     # * revert        (scheduler)        Reverts! assigned absence From accept to applied
 
-    it "Approve! new absence" do
-      absences = @client.approve_absence(43126)
+    fit "Approve! new absence", :vcr do
+      absences = @client.approve_absence(565580)
       expect(absences.state).to eq('approved')
-      assert_requested :patch, staffomatic_url("/absences/#{43126}")
+      assert_requested :patch, staffomatic_url("/absences/#{565580}")
     end
 
-    it "Decline! a new absence" do
-      absences = @client.decline_absence(36957)
+    fit "Decline! a new absence" do
+      absences = @client.decline_absence(565586)
       expect(absences.state).to eq('declined')
-      assert_requested :patch, staffomatic_url("/absences/#{36957}")
+      assert_requested :patch, staffomatic_url("/absences/#{565586}")
     end
 
   end # .update_absences
 
   describe ".create_absence", :vcr do
 
-    it "creates an absence" do
+    fit "creates an absence" do
       absence = @client.create_absence({
         starts_at: "2014-09-20T07:20:00.000+02:00",
         ends_at: "2014-09-26T07:40:00.000+02:00",
-        applicant_id: 493,
-        absence_type_id: 1,
+        applicant_id: 286946,
+        absence_type_id: 14563,
         paid: true,
         vacation: true,
         include_weekends: true,
@@ -75,7 +75,7 @@ describe Staffomatic::Client::Absences do
         commentable: false,
         attachable: false
       })
-      expect(absence.applicant_id).to match(493)
+      expect(absence.applicant_id).to match(286946)
       assert_requested :post, staffomatic_url("absences")
     end
 
